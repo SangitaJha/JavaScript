6dd589e060cb54c0005ccaf7437f333643590f88
Sangita Jha <118240694+SangitaJha@users.noreply.github.com>
2025-07-03 15:29:39 +0000

    math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,13 @@
+const  balance = new Number(100)
+console.log(balance);
+
+//console.log(balance.toString().length));
+
+console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+console.log(Math.random());
+
